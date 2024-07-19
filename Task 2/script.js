95e9a556aa41f3e903c26fc1b77f1fb734c08c2a
lvaisys <lvaisys@gmail.com>
2024-07-19 08:41:21 +0300

    Solution for task 2

---
+++ Task 2/script.js
@@ -0,0 +1,32 @@
+var one = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0
+};
+var two = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0
+};
+two = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0,
+    ugis: 0,
+    svoris: 0
+};
+var three = {
+    vardas: '',
+    pavarde: '',
+    lytis: 'vyras'
+};
+three = {
+    vardas: '',
+    pavarde: '',
+    lytis: 'moteris'
+};
+three = {
+    vardas: '',
+    pavarde: '',
+    lytis: 'sraigtasparnis' // error
+};
